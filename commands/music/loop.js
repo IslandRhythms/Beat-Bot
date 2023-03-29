56ead8918c9aa84088742572329a52275be4b9d8
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2023-03-29 14:54:02 -0400

    rework loop command

--- commands/music/loop.js
+++ commands/music/loop.js
@@ -1,18 +1,24 @@
 const { SlashCommandBuilder } = require('discord.js')
-const { repeat, queue } = require("../../index");
+const { useQueue } = require('discord-player'); 
 
-
+// https://discord-player.js.org/docs/guides/common-actions#looping-the-queue
 module.exports = {
-  data: new SlashCommandBuilder().setName('loop').setDescription('loops the current song'),
+  data: new SlashCommandBuilder().setName('loop').setDescription('loops the current song')
+  .addStringOption(option => option.setName('mode').setDescription('specifies how to loop').setRequired(true)
+  .addChoices({ name: 'off', value: 0}, { name: 'track', value: 1}, { name: 'queue', value: 2 }, { name: 'autoplay', value: 3 })),
   async execute(interaction) {
     // need to check if the user is in the same voice channel as the bot
+    const mode = interaction.option.getString('mode');
     if (!interaction.member.voice.channel) {
       return interaction.reply({ content: 'You must be in a voice channel to use this command', ephemeral: true });
     }
-    const serverQueue = queue.get(interaction.guild.id);
+    if (interaction.guild.members.me.voice.channelId && interaction.member.voice.channelId !== interaction.guild.members.me.voice.channelId) {
+      return interaction.followUp({ content: 'You must be in the same voice channel as me', ephemeral: true });
+    }
+    const serverQueue = useQueue(interaction.guild.id);
     if (!serverQueue) return interaction.reply({ content: 'Nothing to loop', ephemeral: true });
-    if (repeat) return interaction.reply({ content: 'Already looping!', ephemeral: true });
-    repeat = true;
-    return interaction.reply('Will loop chief');
+    console.log('what is serverQueue, trying to find the mode', serverQueue);
+    serverQueue.setRepeatMode(mode);
+    return interaction.reply('Will do chief');
   }
 }
