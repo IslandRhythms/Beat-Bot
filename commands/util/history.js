bc39224c398fcd6d1f2a9557397d9e3c13ace2b4
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2023-03-29 14:58:24 -0400

    update history to not work through dms

--- commands/util/history.js
+++ commands/util/history.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-  data: new SlashCommandBuilder().setName('history').setDescription("Tells you how long you've been a member of the server"),
+  data: new SlashCommandBuilder().setName('history').setDescription("Tells you how long you've been a member of the server").setDMPermission(false),
   async execute(interaction) {
     return interaction.reply(`You joined on: ${new Date(interaction.member.joinedAt).toLocaleDateString()} at ${new Date(interaction.member.joinedAt).toLocaleTimeString()}`);
   }
