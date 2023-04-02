8dfa29065d409dea0579054748306b216ddba7fd
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2023-04-01 23:10:33 -0400

    Possible route that we want

---
+++ getAPIRoutePermissions.js
@@ -0,0 +1,29 @@
+const config = require('./config.json');
+const fetch = require('node-fetch');
+
+async function run() {
+  const res = await fetch(`http://api.steampowered.com/ISteamWebAPIUtil/GetSupportedAPIList/v0001/?key=${config.steamAPIKEY}`).then(res => res.json());
+  let apis = res.apilist.interfaces;
+  console.log('The apis I have permission to are?', apis);
+  // get the ones I care about
+  const care = [];
+  for (let i = 0; i < apis.length; i++) {
+    if (apis[i].name.includes('Steam') || apis[i].name.includes('Auth')) {
+      care.push(apis[i]);
+    }
+  }
+  console.log('the ones we care about', care);
+  // print the methods
+  console.log('there name and methods')
+  care.forEach( item => {
+    console.log('====================================')
+    console.log(item.name, item.methods);
+    const index = item.methods.find(entry => entry.name == 'GetCollectionDetails')
+    console.log('what is index?', index);
+    if (index) {
+      console.log('what are the params?', index);
+    }
+  });
+}
+
+run();
