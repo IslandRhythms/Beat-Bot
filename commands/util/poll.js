dd70892db687df9c5963fa4bc2489d57268221a4
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2023-04-01 22:38:50 -0400

    Bugs fixes and improvements
    fixed bug in advice.js where it was `followup()` instead of `followUp()`
    fixed table command to use left instead of center or right as it was causing issues rendering the table
    added role mentionable to poll command to allow for more efficient messaging

--- commands/util/poll.js
+++ commands/util/poll.js
@@ -4,7 +4,8 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder().setName('poll').setDescription('starts a poll on the given topic')
   .addStringOption(option => option.setName('question').setDescription('the topic in the form of a question').setRequired(true))
-  .addStringOption(option => option.setName('choices').setDescription('the possible choices, max 20, for the poll in the form of comma separated values')),
+  .addStringOption(option => option.setName('choices').setDescription('the possible choices, max 20, for the poll in the form of comma separated values'))
+  .addRoleOption(option => option.setName('audience').setDescription('the people intended to respond to the poll')),
   async execute(interaction) {
     await interaction.deferReply();
     let question = interaction.options.getString('question');
@@ -12,9 +13,10 @@ module.exports = {
       question = question + '?'
     }
     const choices = interaction.options.getString('choices');
+    const audience = interaction.options.getRole('audience') ?? '';
     let queueEmbed = new EmbedBuilder().setColor("#ff7373").setTitle('📊 '+ question);
     if (!choices) {
-      await interaction.followUp({ embeds: [queueEmbed] });
+      await interaction.followUp({ content: audience, embeds: [queueEmbed] });
       const msg = await interaction.fetchReply();
       await msg.react('👍');
       await msg.react('👎');
