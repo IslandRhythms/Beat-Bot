541954a7a474a9d2494908037c06d25344b857b2
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2023-04-01 23:12:02 -0400

    docs: left note as to what first call does

--- getAPIRoutePermissions.js
+++ getAPIRoutePermissions.js
@@ -2,6 +2,7 @@ const config = require('./config.json');
 const fetch = require('node-fetch');
 
 async function run() {
+  // check what routes this key grants
   const res = await fetch(`http://api.steampowered.com/ISteamWebAPIUtil/GetSupportedAPIList/v0001/?key=${config.steamAPIKEY}`).then(res => res.json());
   let apis = res.apilist.interfaces;
   console.log('The apis I have permission to are?', apis);
