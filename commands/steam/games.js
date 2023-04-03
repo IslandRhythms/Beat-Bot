8a799c3d1148d2c8fbd471a38ca089b243ca0dcf
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2023-04-03 18:22:24 -0400

    command complete

--- commands/steam/games.js
+++ commands/steam/games.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
-const path = require('path');
 const fs = require('fs');
 const VDF = require('vdf-parser');
+const fetch = require('node-fetch');
 
 const collectionFile = fs.readFileSync('C:\\Program Files (x86)\\Steam\\userdata\\256486405\\7\\remote\\sharedconfig.vdf').toString();
 const res = VDF.parse(collectionFile);
@@ -9,16 +9,36 @@ const res = VDF.parse(collectionFile);
 module.exports = {
   data: new SlashCommandBuilder().setName('recgames').setDescription('lists the games that I currently find fun to play'),
   async execute(interaction) {
-    console.log('what is res', res);
+    await interaction.deferReply({ ephemeral: true });
     const apps = res.UserRoamingConfigStore.Software.Valve.Steam.apps;
-    console.log('what is the obj we want', apps)
+    const games = [];
+    let str = "Here are some games that I've found to be super fun.\n";
     for (const key in apps) {
       if (Object.keys(apps[key]).length > 0) {
         if (apps[key].tags) {
-          console.log('the key is', key, 'the value is', apps[key], 'what is in tags', Object.values(apps[key]?.tags));
+          // https://stackoverflow.com/questions/57031685/how-can-i-get-details-about-an-steam-appid-from-steam-web-api
+          // This isn't even official like god damn
+          const test = await fetch(`https://store.steampowered.com/api/appdetails?appids=${key}`).then(res => res.json());
+          const obj = {};
+          obj.name = test[key].data.name; // cannot use dot notation for string numbers I guess
+          obj.appId = key;
+          obj.shortDescription = test[key].data.short_description;
+          console.log('what is genres', test[key].data.genres)
+          obj.genres = test[key].data.genres.map(({id, ...item}) => item);
+          obj.link = `store.steampowered.com/app/${key}`;
+          games.push(obj);
+
         }
       }
     }
-    await interaction.reply('I do not do anything right now')
+    // now to format the string;
+    await interaction.followUp(str)
+    // There's a length requirement. So have to do it like this.
+    for (let i = 0; i < games.length; i++) {
+      const genres = [];
+      games[i].genres.forEach(item => genres.push(item.description));
+      await interaction.channel.send(`${games[i].name}\n${games[i].shortDescription}\n${genres.join(', ')}\n${games[i].link}\n===============================\n`);
+      genres.length = 0;
+    }
   }
 }
