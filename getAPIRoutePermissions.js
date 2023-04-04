2591473c9cb4d7d92f30e304d600d452e94f553b
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2023-04-04 15:41:56 -0400

    left notes

--- getAPIRoutePermissions.js
+++ getAPIRoutePermissions.js
@@ -1,6 +1,8 @@
 const config = require('./config.json');
 const fetch = require('node-fetch');
 
+const { Timer } = require('timer-node');
+
 async function run() {
   // check what routes this key grants
   const res = await fetch(`http://api.steampowered.com/ISteamWebAPIUtil/GetSupportedAPIList/v0001/?key=${config.steamAPIKEY}`).then(res => res.json());
@@ -43,10 +45,22 @@ getNews();
 */
 
 // Is this readable in memory? I guess it is. Use for discount command.
-// multiple results? Have user pic somehow.
+// multiple results? Have user pick somehow.
+// Runtime on the PC is 2 ms. It might run slower on the pi.
 async function getGames() {
   const res = await fetch(`https://api.steampowered.com/ISteamApps/GetAppList/v2/?key=${config.steamAPIKEY}`).then(res => res.json());
   console.log('what is res', res.applist.apps);
+  const timer = new Timer({ label: 'runtime calculation'});
+  timer.start();
+  console.log(timer.isRunning());
+  let j = 0;
+  for (let i = 0; i < res.applist.apps.length; i++) {
+    j++;
+  }
+  console.log('what is j', j);
+  console.log(timer.time());
+  timer.stop();
+  console.log('done');
 }
 
 getGames();
