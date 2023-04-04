cb95d198b2fc659cae10e5ab6e6dc037874859ed
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2023-04-04 15:24:56 -0400

    To use embeds or not to use embeds

--- commands/steam/deals.js
+++ commands/steam/deals.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const fetch = require('node-fetch');
 
 module.exports = {
@@ -6,27 +6,33 @@ module.exports = {
   async execute(interaction) {
     await interaction.deferReply({ ephemeral: true });
     const res = await fetch('http://store.steampowered.com/api/featuredcategories/?cc=us&l=en').then(res => res.json());
-    console.log('what is res', res);
     let dailyDeal = '';
+    let queueEmbed = new EmbedBuilder()
+    .setColor("#ff7373")
+    .setTitle(`**Steam Deals**`);
     let str = '**Daily Deals**\n';
+    queueEmbed.addFields({ name: str, value: '===================='})
     const keys = Object.keys(res);
     const categories = keys.filter(x => !Number.isNaN(parseInt(x)))
     for (let i = 0; i < categories.length; i++) {
       if (res[i].name == 'Daily Deal') {
         dailyDeal = res[i];
         dailyDeal.items.forEach(entry => {
-          str += `${entry.name}: $${entry.original_price/100} => $${entry.final_price/100} (${entry.discount_percent}% off)\nhttps://store.steampowered.com/app/${entry.id}\n`;
+          // str += `${entry.name}: $${entry.original_price/100} => $${entry.final_price/100} (${entry.discount_percent}% off)\nhttps://store.steampowered.com/app/${entry.id}\n`;
+          queueEmbed.addFields({ name: `${entry.name}: $${entry.original_price/100} => $${entry.final_price/100} (${entry.discount_percent}% off)\n`, value: `https://store.steampowered.com/app/${entry.id}\n`})
         });
         break;
       }
     }
-    console.log('what is res.specials.items', res.specials.items);
-    str += '**Specials**\n';
+    // str += '**Specials**\n';
+    str = '**Specials**\n';
+    queueEmbed.addFields({ name: str, value: '============================='})
     // format the string
     for (let i = 0; i < res.specials.items.length; i++) {
       const game = res.specials.items[i];
-      str += `${game.name}: $${game.original_price/100} => $${game.final_price/100} (${game.discount_percent}% off)\nhttps://store.steampowered.com/app/${game.id}\n`;
+      // str += `${game.name}: $${game.original_price/100} => $${game.final_price/100} (${game.discount_percent}% off)\nhttps://store.steampowered.com/app/${game.id}\n`;
+      queueEmbed.addFields({ name: `${game.name}: $${game.original_price/100} => $${game.final_price/100} (${game.discount_percent}% off)`, value: `https://store.steampowered.com/app/${game.id}`})
     }
-    await interaction.followUp(str)
+    await interaction.followUp({embeds:[queueEmbed]});
   }
 }
