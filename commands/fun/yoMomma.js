6133c49f7fb931fc2e3600ff7d28ba98a6453fc2
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2024-01-15 20:16:16 -0500

    api was taken offline but left all jokes in a file

--- commands/fun/yoMomma.js
+++ commands/fun/yoMomma.js
@@ -1,18 +1,17 @@
 const { SlashCommandBuilder } = require('discord.js');
-const axios = require('axios');
+const yoMommaJokes = require('../../jokes.json');
 
 module.exports = {
   data: new SlashCommandBuilder().setName('yomomma').setDescription('sends a yo momma joke')
   .addUserOption(option => option.setName('target').setDescription('the person to send a you momma joke to.')),
   async execute(interaction) {
     await interaction.deferReply();
-    const res = await axios.get('https://yomomma-api.herokuapp.com/jokes').then(res => res.data);
-    console.log(res);
     const user = interaction.options.getUser('target') ?? '';
+    const joke = yoMommaJokes[Math.floor(Math.random() * yoMommaJokes.length)];
     if (user) {
-      return interaction.followUp(`${user} ${res.joke}`);
+      return interaction.followUp(`${user} ${joke}`);
     } else {
-      return interaction.followUp(`${res.joke}`);
+      return interaction.followUp(`${joke}`);
     }
   }
 }
