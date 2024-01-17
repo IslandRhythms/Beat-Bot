d9360cda227b63598bb64370edb5f0268c27b9ed
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2024-01-17 00:55:34 -0500

    complete pokemon

--- commands/fun/pokemon.js
+++ commands/fun/pokemon.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const axios = require('axios');
+const capitalizeFirstLetter = require('../../helpers/capitalizeFirstLetter');
 
 // https://discord.js.org/#/docs/builders/main/class/SlashCommandBuilder?scrollTo=addMentionableOption
 
@@ -12,8 +13,24 @@ module.exports = {
     const pokemon = interaction.options.getString('pokemon');
     const embed = new EmbedBuilder();
     const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`).then(res => res.data);
-    console.log('what is res', res);
-    await interaction.followUp('Under Construction');
-    // await interaction.followUp({ embeds: [embed] })
+    // two different payloads
+    const pokedex = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${res.name}`).then(res => res.data);
+    const nationalDex = pokedex.pokedex_numbers.find(x => x.pokedex.name == 'national');
+    let evolvesFrom = '';
+    const name = capitalizeFirstLetter(res.name);
+    if (pokedex.evolves_from_species != null) {
+      evolvesFrom = `${name}, the evolved form of ${pokedex.evolves_from_species.name}. `
+    }
+    embed.setAuthor({ name: 'pokeapi', iconURL: 'https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_64.png', url: 'https://pokeapi.co/' });
+    const genus = pokedex.genera.find(x => x.language.name == 'en');
+    embed.setTitle(`${name}, The ${genus.genus}. `);
+    embed.setDescription(`${evolvesFrom}${pokedex.flavor_text_entries[0].flavor_text}`);
+    for (let i = 0; i < res.types.length;i++) {
+      embed.addFields({ name: 'Type', value: res.types[i].type.name, inline: true });
+    }
+    embed.addFields({ name: 'National Dex Number', value: nationalDex.entry_number.toString() });
+    embed.setImage(res.sprites.front_default);
+    embed.setFooter({ text: 'Possible thanks to the pokemon api https://pokeapi.co/' });
+    await interaction.followUp({ embeds: [embed] })
   }
 }
