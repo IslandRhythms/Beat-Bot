78357b5f608d51adddc748703f36d015bd03061e
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2024-02-08 23:13:09 -0500

    new command plus notes

--- commands/tabletop/generateSheet.js
+++ commands/tabletop/generateSheet.js
@@ -1,11 +1,15 @@
 
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, MessageAttachment } = require('discord.js');
+const fs = require('fs');
 
 module.exports = {
   data: new SlashCommandBuilder().setName('generatesheet')
   .setDescription('generates an html page to enter character information'),
   async execute(interaction) {
     await interaction.deferReply();
-    await interaction.followUp('Under Construction')
+    await interaction.followUp({
+      content: 'Here you go! Download the file, make your changes, save, and then upload using /uploadCharacter',
+      files: [{ attachment: __dirname+'../../../tableTopCharacterInformation.html', name: `${interaction.user.username}.html` }]
+    });
   }
 }
--- helpers/parseHtmlPage.js
+++ helpers/parseHtmlPage.js
@@ -6,4 +6,17 @@ const axios = require('axios');
 module.exports = async function parseHtmlPage(sheet) {
   const res = await axios.get(sheet.url).then(res => res.data);
   const root = parser.parse(res);
+  const inputs = root.querySelectorAll('input');
+  const textarea = root.querySelectorAll('textarea');
+  let multiclass = false;
+  // change all the ids to match the db property names for easier parsing
+  const obj = {};
+  inputs.forEach(input => {
+
+  });
+  textarea.forEach(input => {
+
+  });
+  console.log('parsed obj', obj);
+  return obj;
 }
