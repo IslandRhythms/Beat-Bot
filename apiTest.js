66637d25a42553cea49f4e5fd7c6f2b08e80291f
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2024-02-28 23:29:33 -0500

    prep sports

--- apiTest.js
+++ apiTest.js
@@ -2,8 +2,16 @@ const axios = require('axios');
 
 
 async function run() {
-  const res = await axios.get(`https://date.nager.at/api/v3/PublicHolidays/2024/US`).then(res => res.data);
-  console.log('what is res', res);
+  const res = await axios.get(`https://site.web.api.espn.com/apis/site/v2/sports/basketball/nba/teams/14`).then(res => res.data);
+  console.log('what is res', res, res.team.nextEvent);
+
+  // doesn't work for whatever reason
+  const test = await axios.get(`https://site.api.espn.com/apis/site/v2/sports/basketball/nba/14/schedule`).then(res => res.data).catch(e => console.log(e.code))
+  
+  const response = await axios.get(`https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard`).then(res => res.data)
+
+  console.log('what is response', response)
+  // const teams = await axios.get(`https://site.api.espn.com/apis/site/v2/sports/basketball/nba/miami-heat/roster`)
 }
 
 run();
--- commands/util/setBirthday.js
+++ commands/util/setBirthday.js
@@ -1,20 +1,23 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const months = require('../../calendarMonths.json');
+const jsMonths = months.map(x => x.value -= 1);
+const minimumYear = 1940;
 
 // should probably be number options
+// Search on google: chinese zodiac descriptions printable
+// and have that as part of the happy birthday message
 module.exports = {
   data: new SlashCommandBuilder().setName('setbirthday')
-  .setDescription('tell beat bot when your birthday is so he can wish you a happy birthday.')
-  .addStringOption(option => option.setName('month').setDescription('the month you were born').addChoices(
-
-  ))
-  .addStringOption(option => option.setName('day').setDescription('the day you were born').addChoices(
-
-  ))
-  .addStringOption(option => option.setName('year').setDescription('the year you were born').addChoices(
-
-  )),
+  .setDescription('tell beat bot when your birthday is so he can wish you a happy birthday and other things.')
+  .addNumberOption(option => option.setName('month').setDescription('the month you were born').addChoices(...jsMonths).setRequired(true))
+  .addNumberOption(option => option.setName('day').setDescription('the day you were born').setMinValue(1).setMaxValue(31).setRequired(true))
+  .addNumberOption(option => option.setName('year').setDescription('the year you were born').setMinValue(minimumYear).setMaxValue(new Date().getFullYear() - 15).setRequired(true)),
   async execute(interaction) {
     await interaction.deferReply();
+    const month = interaction.options.getNumber('month');
+    const day = interaction.options.getNumber('day');
+    const year = interaction.options.getYear('year');
+    const date = new Date(year, month, day);
     await interaction.followUp();
   }
 }
