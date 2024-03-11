bb4bf9cac6f18157556c6eb11a26648701f42e58
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2024-03-10 21:47:15 -0400

    getting objectId not found occasionally, why?

--- apiTest.js
+++ apiTest.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-
+/*
 async function run() {
   const res = await axios.get(`https://site.web.api.espn.com/apis/site/v2/sports/basketball/nba/teams/14`).then(res => res.data);
   console.log('what is res', res, res.team.nextEvent);
@@ -22,3 +22,25 @@ async function run() {
 }
 
 run();
+
+*/
+
+// might have to keep spamming api to get object with image for the met
+async function met() {
+  const res = await axios.get('https://collectionapi.metmuseum.org/public/collection/v1/objects').then(res => res.data);
+  console.log('what is res', res);
+  const selectedArtwork = Math.floor(Math.random() * res.total) + 1; // ensure 0 is not called
+  console.log('what is selectedArtwork', selectedArtwork)
+  const artwork = await axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${selectedArtwork}`).then(res => res.data).catch(e => console.log('what is e', e));
+  console.log('what is artwork', artwork);
+}
+
+async function chicago() {
+  console.log('=======================================================');
+  const { pagination, data} = await axios.get(`https://api.artic.edu/api/v1/artworks?limit=2`).then(res => res.data);
+  console.log('what is pagination', pagination);
+  console.log('what is data', data[0])
+}
+
+met();
+chicago();
