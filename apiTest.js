497f5239a9fc94760d9987378e3bb85200b5add2
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2024-03-17 01:03:36 -0400

    update help  to jump to each category section

--- apiTest.js
+++ apiTest.js
@@ -1,19 +1,19 @@
 const axios = require('axios');
 require('./config');
 
-/*
+
 async function run() {
   const res = await axios.get(`https://site.web.api.espn.com/apis/site/v2/sports/basketball/nba/teams/14`).then(res => res.data);
   console.log('what is res', res, res.team.nextEvent);
-
+  console.log('===============================')
   // doesn't work for whatever reason
-  const test = await axios.get(`https://site.api.espn.com/apis/site/v2/sports/basketball/nba/14/schedule`).then(res => res.data).catch(e => console.log(e.code))
-  
+  const test = await axios.get(`https://site.web.api.espn.com/apis/site/v2/sports/basketball/nba/14/schedule`).then(res => res.data).catch(e => console.log(e.code))
+  console.log('===============================')
   const response = await axios.get(`https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard`).then(res => res.data)
-
+  console.log('===============================')
   console.log('what is response', response)
   // const teams = await axios.get(`https://site.api.espn.com/apis/site/v2/sports/basketball/nba/miami-heat/roster`)
-
+  console.log('===============================')
   const teams = await axios.get(`https://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams`).then(res => res.data.sports[0]);
   console.log('========')
   console.log(teams);
@@ -24,23 +24,22 @@ async function run() {
 
 run();
 
-*/
+// async function run() {
+//   // free tier only allows up to id 3000
+//   /*
+//   let url = `https://perenual.com/api/species-list?key=${process.env.PLANTAPIKEY}`;
+//   const { last_page } = await axios.get(url).then(res => res.data);
+//   const selectedPage = Math.floor(Math.random() * last_page) + 1; // +1 because there is no page 0
+//   url += `&page=${selectedPage}`;
+//   const { data } = await axios.get(url).then(res => res.data).catch(e => console.log(e.message));
+//   const selectedPlantIndex = Math.floor(Math.random() * data.length);
+//   console.log('The plant of the day is', data[selectedPlantIndex]);
+//   const plantOfTheDay = data[selectedPlantIndex];
+//   */
+//   console.log('Getting plant information ...');
+//   const plantOfTheDayId = Math.floor(Math.random() * 3000) + 1;
+//   const plantInformation = await axios.get(`https://perenual.com/api/species/details/${plantOfTheDayId}?key=${process.env.PLANTAPIKEY}`).then(res => res.data).catch(e => console.log(e.message));
+//   console.log(plantInformation);
+// }
 
-async function run() {
-  let url = `https://perenual.com/api/species-list?key=${process.env.PLANTAPIKEY}`;
-  const { last_page } = await axios.get(url).then(res => res.data);
-  const selectedPage = Math.floor(Math.random() * last_page) + 1; // +1 because there is no page 0
-  url += `&page=${selectedPage}`;
-  const { data } = await axios.get(url).then(res => res.data);
-  const selectedPlantIndex = Math.floor(Math.random() * data.length);
-  console.log('The plant of the day is', data[selectedPlantIndex]);
-  const plantOfTheDay = data[selectedPlantIndex];
-  console.log('Getting plant information ...');
-  const plantInformation = await axios.get(`https://perenual.com/api/species/details/${plantOfTheDay.id}?key=${process.env.PLANTAPIKEY}`).then(res => res.data);
-  console.log(plantInformation);
-  // what to do with this?
-  const plantDisease = await axios.get(`https://perenual.com/api/pest-disease-list?key=${process.env.PLANTAPIKEY}`).then(res => res.data);
-  console.log(plantDisease.data[0])
-}
-
-run();
+// run();
--- commands/util/help.js
+++ commands/util/help.js
@@ -1,8 +1,8 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ActionRowBuilder, ComponentType, ButtonBuilder } = require('discord.js');
 const path = require('path');
 const fs = require('fs');
 const capitalizeFirstLetter = require('../../helpers/capitalizeFirstLetter');
-const { Pagination } = require('pagination.djs');
+const { Pagination, ExtraRowPosition } = require('pagination.djs');
 const categorySummaries = require('../../categorySummaries.json'); // be sure to update the file if a new folder is added
 module.exports = {
   data: new SlashCommandBuilder().setName('help').setDescription('Lists the commands of the bot'),
@@ -28,10 +28,13 @@ module.exports = {
     }
     const embeds = [];
     let totalCommands = 0;
+    const row = new ActionRowBuilder();
+    const selectMenu = new StringSelectMenuBuilder().setCustomId('categorySelect').setPlaceholder('Jump to ...')
     for (let i = 0; i < commandFolders.length; i++) {
       const embed = new EmbedBuilder();
       const category = commandFolders[i];
       embed.setTitle(capitalizeFirstLetter(category));
+      selectMenu.addOptions(new StringSelectMenuOptionBuilder().setLabel(capitalizeFirstLetter(category)).setValue(capitalizeFirstLetter(category)));
       for (let index = 0; index < commands.length; index++) {
         const command = commands[index];
         if (command.category == category) {
@@ -42,11 +45,43 @@ module.exports = {
       }
       embeds.push(embed);
     }
+
     pagination.setEmbeds(embeds, (currentEmbed, index, array) => {
       currentEmbed.setFooter({ text: `Page ${index + 1} / ${array.length} Total available commands ${totalCommands}`});
       return currentEmbed.setAuthor({ name: `Beat Bot Available Commands`});
     });
     pagination.setOptions({ ephemeral: true });
-    pagination.render();
+    row.addComponents(selectMenu);
+    // for some reason, it can't render the letter symbols
+    pagination.buttons = {
+      // first: new ButtonBuilder().setCustomId('first').setLabel('/U0001F1ED').setEmoji(':regional_indicator_h:').setDisabled(true).setStyle('Success'),
+      // last: new ButtonBuilder().setCustomId('last').setLabel('🇪').setDisabled(true).setStyle('Success'),
+      // next: new ButtonBuilder().setCustomId('next').setLabel('🇱').setDisabled(true).setStyle('Success'),
+      // prev: new ButtonBuilder().setCustomId('prev').setEmoji(':parking:').setDisabled(true).setStyle('Success'),
+      extra: new ButtonBuilder().setCustomId('exclaim').setEmoji('❕').setDisabled(true).setStyle('Success'),
+    }
+    pagination.addActionRows([row], ExtraRowPosition.Above)
+    const msg = await pagination.render();
+    // bot cannot react to ephemeral messages
+    // doing this breaks the default buttons
+    const collector = await msg.createMessageComponentCollector({ componentType: ComponentType.StringSelect, filter: (i) => i.user.id === interaction.user.id, time: 360_000 });
+
+    collector.on('collect', async i => {
+      const index = embeds.findIndex(x => x.data.title == i.values[0]);
+      if (index != -1) {
+        pagination.goToPage(index + 1);
+        pagination.ready();
+        await pagination.editReply(pagination);
+        await i.reply({ content: `Jumped to ${i.values}`, ephemeral: true });
+        setTimeout(async () => {
+          await i.deleteReply();
+        }, 3000)
+      } else {
+        await i.reply({ content: `Category ${i.values} could not be found`, ephemeral: true });
+        setTimeout(async () => {
+          await i.deleteReply();
+        }, 500)
+      }
+    });
   }
 }
