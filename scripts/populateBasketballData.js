3c060f975bfad2a5234c8d2216ba65dff81e4fc3
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2024-03-22 02:40:55 -0400

    begin scripts for autocomplete

--- scripts/populateBasketballData.js
+++ scripts/populateBasketballData.js
@@ -27,18 +27,13 @@ async function run() {
   const  { response } = await axios(config).then(res => res.data);
   const NBA = response.find(x => x.name == 'NBA');
   const NCAA = response.find(x => x.name == 'NCAA');
-  console.log('what is NBA', NBA, 'what is NCAA', NCAA);
   const basketballData = { NBA: { id: NBA.id, logo: NBA.logo, seasons: NBA.seasons, countryId: NBA.country.id, teams: [] },
     NCAA: { id: NCAA.id, logo: NCAA.logo, seasons: NCAA.seasons, countryId: NCAA.country.id, teams: [] }
   };
   const keys = Object.keys(basketballData);
   for (let i = 0; i < keys.length; i++) {
-    console.log(basketballData[keys[i]].id)
-    // 
     config.url = `https://v1.basketball.api-sports.io/teams?league=${basketballData[keys[i]].id}&country_id=${basketballData[keys[i]].countryId}&season=${basketballData[keys[i]].seasons[0].season}`;
-    console.log('what is config', config);
     const teamsData = await axios(config).then(res => res.data).catch(e => e.code);
-    console.log('what is teamsData', teamsData);
     const reformattedData = teamsData.response.map(x => ({ id: x.id, name: x.name, logo: x.logo }));
     basketballData[keys[i]].teams = reformattedData;
   }
---
+++ scripts/populateFootballData.js
@@ -0,0 +1,46 @@
+'use strict';
+const axios = require('axios');
+const fs = require('fs/promises');
+require('../config');
+
+run().catch(e => {
+  console.log(e);
+  process.exit(-1)
+})
+
+async function run() {
+  if (!process.argv[2]) {
+    throw new Error('Must provide fileName');
+  }
+  // include file type
+  const fileName = process.argv[2];
+
+  const config = {
+    method: 'GET',
+    url: 'https://v1.american-football.api-sports.io/leagues',
+    headers: {
+      'x-rapidapi-key': process.env.SPORTSAPIKEY,
+      'x-rapidapi-host': 'v1.american-football.api-sports.io'
+    }
+  };
+
+  const  { response } = await axios(config).then(res => res.data);
+  const NFL = response.find(x => x.league.name == 'NFL');
+  const NCAA = response.find(x => x.league.name == 'NCAA');
+
+  const footballData = { NFL: { id: NFL.league.id, logo: NFL.league.logo, seasons: NFL.seasons, teams: [] },
+    NCAA: { id: NCAA.league.id, logo: NCAA.league.logo, seasons: NCAA.seasons, teams: [] }
+  };
+  const keys = Object.keys(footballData);
+  for (let i = 0; i < keys.length; i++) {
+    config.url = `https://v1.american-football.api-sports.io/teams?league=${footballData[keys[i]].id}&season=${footballData[keys[i]].seasons[0].year}`;
+    const teamsData = await axios(config).then(res => res.data).catch(e => e.code);
+    const reformattedData = teamsData.response.map(x => ({ id: x.id, name: x.name, logo: x.logo }));
+    footballData[keys[i]].teams = reformattedData;
+  }
+
+
+  
+  await fs.writeFile(`../${fileName}`, JSON.stringify(footballData, null, 2))
+  console.log('done');
+}
