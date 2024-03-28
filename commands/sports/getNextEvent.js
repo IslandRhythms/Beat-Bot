29191a72e7040526643a2b74b67dcfd9e9360f1c
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2024-03-27 20:45:47 -0400

    common autocomplete function for sports

--- commands/sports/getNextEvent.js
+++ commands/sports/getNextEvent.js
@@ -6,6 +6,7 @@ const Baseball = require('../../sportsData/Baseball.json');
 const Hockey = require('../../sportsData/Hockey.json');
 const Soccer = require('../../sportsData/Soccer.json')
 const Jimp = require('jimp');
+const sportsAutocomplete = require('./common/autocomplete.js')
 
 module.exports = {
   cooldown: 30,
@@ -155,50 +156,7 @@ module.exports = {
     await interaction.followUp({ embeds: [embed], files: [{ attachment: nextGame.outputPath, name: `${nextGame.fileName}`}]});
   },
   async autocomplete(interaction) {
-    let focusedValue = interaction.options.getFocused(true);
-    // initial whitespace is causing it to send all results which results in a crash since limit is 25
-    if (focusedValue.value == '') {
-      focusedValue = null;
-    } else {
-      focusedValue = focusedValue.value.toLowerCase();
-    }
-    const sport = interaction.options._subcommand;
-    let teams = [];
-    // if league is not provided, it needs to list all teams in both leagues.
-    if (sport == 'basketball') {
-      const keys = Object.keys(Basketball);
-      for (let i = 0; i < keys.length; i++) {
-        teams = teams.concat(Basketball[keys[i]].teams)
-      }
-    } else if (sport == 'football') {
-      const keys = Object.keys(Football);
-      for (let i = 0; i < keys.length; i++) {
-        teams = teams.concat(Football[keys[i]].teams)
-      }
-    } else if (sport == 'hockey') {
-      const keys = Object.keys(Hockey);
-      for (let i = 0; i < keys.length; i++) {
-        teams = teams.concat(Hockey[keys[i]].teams)
-      }
-    } else if (sport == 'baseball') {
-      const keys = Object.keys(Baseball);
-      for (let i = 0; i < keys.length; i++) {
-        teams = teams.concat(Baseball[keys[i]].teams)
-      }
-    } else if (sport == 'soccer') {
-      const keys = Object.keys(Soccer);
-      for (let i = 0; i < keys.length; i++) {
-        teams = teams.concat(Soccer[keys[i]].teams)
-      }
-    }
-
-		let filtered = teams.filter(choice => choice.name.toLowerCase().includes(focusedValue));
-    if (filtered.length > 25) {
-      filtered = filtered.slice(0, 25);
-    }
-		await interaction.respond(
-			filtered.map(choice => ({ name: choice.name, value: choice.name })),
-		);
+    return sportsAutocomplete(interaction);
   }
 }
 
