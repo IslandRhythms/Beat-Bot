9d4d165059fa0fa5016af57b4701df2a9fa8d4a8
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2024-04-30 20:51:38 -0400

    Update getSchedule.js

--- commands/sports/getSchedule.js
+++ commands/sports/getSchedule.js
@@ -1,5 +1,15 @@
-'use strict';
+const { SlashCommandBuilder } = require('discord.js');
+const axios = require('axios');
+const commandDef = require('./common/commandDef');
+const sportsAutocomplete = require('./common/autocomplete');
 
 module.exports = {
-  
+  data: commandDef('schedule', 'Get your sports team\'s schedule for the year.'),
+  async autocomplete(interaction) {
+    return sportsAutocomplete(interaction);
+  },
+  async execute(interaction) {
+    await interaction.deferReply();
+    await interaction.followUp('Under Construction');
+  }
 }
