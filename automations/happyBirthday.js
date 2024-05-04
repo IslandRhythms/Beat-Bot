32c05018d2e87be902b825948dac60c86b224bec
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2024-05-04 15:54:48 -0400

    Update happyBirthday.js

--- automations/happyBirthday.js
+++ automations/happyBirthday.js
@@ -1,4 +1,5 @@
 'use strict';
+const wikipedia = require('wikipedia');
 
 module.exports = async function happyBirthday(db, bot) {
   const { User } = db.models;
@@ -24,5 +25,14 @@ module.exports = async function happyBirthday(db, bot) {
     const age = todayYear - birthYear;
     console.log(`Happy birthday, ${user.name}! You are now ${age} years old.`);
   });
+
+  const events = await wikipedia.onThisDay();
+  const choices = Object.keys(events);
+  const index = Math.floor(Math.random() * choices.length);
+  const selectedArray = events[choices[index]];
+  const onThisDayIndex = Math.floor(Math.random() * selectedArray.length);
+  const pageEntry = selectedArray[onThisDayIndex].pages[0];
+  const history = `For ${choices[index]} on this day, ${selectedArray[onThisDayIndex].year ? `in the year ${selectedArray[onThisDayIndex].year},` : ''} ${selectedArray[onThisDayIndex].text}
+  link: ${pageEntry.content_urls.mobile.page}`;
   // send message into channel here / maybe send a DM?
 }
