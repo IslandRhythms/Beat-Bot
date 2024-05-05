6903a7d2fac8d0043bb4a7b33467c65a69200192
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2024-05-05 19:49:00 -0400

    restart poll.js

--- commands/util/poll.js
+++ commands/util/poll.js
@@ -8,50 +8,6 @@ module.exports = {
   .addRoleOption(option => option.setName('audience').setDescription('the people intended to respond to the poll')),
   async execute(interaction, conn) {
     await interaction.deferReply();
-    const { Poll } = conn.models;
-    let question = interaction.options.getString('question');
-    if (!question.endsWith('?')) {
-      question = question + '?'
-    }
-    const choices = interaction.options.getString('choices');
-    const audience = interaction.options.getRole('audience') ?? '';
-    let queueEmbed = new EmbedBuilder().setColor("#ff7373").setTitle('📊 '+ question);
-    const numPolls = await Poll.estimatedDocumentCount();
-    const pollDoc = new Poll({ pollId: numPolls });
-    if (audience) {
-      pollDoc.target = `${audience}`;
-      audience.members.map(x => pollDoc.eligibleVoters.push(x.user.id));
-    }
-    if (!choices) {
-      await interaction.followUp({ content: `What do you think? ${audience}`, embeds: [queueEmbed] });
-      const msg = await interaction.fetchReply();
-      await msg.react('👍');
-      await msg.react('👎');
-      console.log('numPolls', numPolls, 'what is msg payload', msg);
-      pollDoc.messageLink = `https://discord.com/channels/${msg.guildId}/${msg.channelId}/${msg.id}`;
-      pollDoc.messageId = msg.id;
-      pollDoc.question = msg.content;
-      pollDoc.isBinary = true;
-      await pollDoc.save();
-    } else {
-      const options = choices.split(',');
-      const numLoops = options.length > 20 ? 20: options.length;
-      const alphabet = ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬', '🇭', '🇮', '🇯', '🇰', '🇱',
-            '🇲', '🇳', '🇴', '🇵', '🇶', '🇷', '🇸', '🇹', '🇺', '🇻', '🇼', '🇽', '🇾', '🇿'];
-      for (let i = 0; i < numLoops; i++) {
-        queueEmbed.addFields({ name: alphabet[i] + ' ' + options[i], value: ' ', inline: true });
-      }
-      const msg = await interaction.followUp({ content: `What do you think? ${audience}`, embeds: [queueEmbed] });
-      pollDoc.messageLink = `https://discord.com/channels/${msg.guildId}/${msg.channelId}/${msg.id}`;
-      pollDoc.messageId = msg.id;
-      pollDoc.question = msg.content;
-      pollDoc.isBinary = false;
-      pollDoc.choices = options;
-      await pollDoc.save();
-      for (let i = 0; i < numLoops; i++) {
-        await msg.react(alphabet[i])
-      }
-    }
-    
+    await interaction.followUp(`Under construction`)
   }
 }
