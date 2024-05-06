8116fc3b3acc54536e7139263440a08959415473
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2024-05-05 20:30:05 -0400

    everything looks right but it doesn't work

--- commands/util/poll.js
+++ commands/util/poll.js
@@ -1,13 +1,37 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
+const { emojiCharacters } = require('../../resources/constants');
+
 
 module.exports = {
   data: new SlashCommandBuilder().setName('poll').setDescription('starts a poll on the given topic')
   .addStringOption(option => option.setName('question').setDescription('the topic in the form of a question').setRequired(true))
-  .addStringOption(option => option.setName('choices').setDescription('the possible choices, max 20, for the poll in the form of comma separated values'))
-  .addRoleOption(option => option.setName('audience').setDescription('the people intended to respond to the poll')),
-  async execute(interaction, conn) {
+  .addStringOption(option => option.setName('choices').setDescription('the possible choices, max 10, for the poll in the form of comma separated values'))
+  .addRoleOption(option => option.setName('audience').setDescription('the people intended to respond to the poll'))
+  .addBooleanOption(option => option.setName('multiple').setDescription('true to allow multiple answers. Default is no.'))
+  .addNumberOption(option => option.setName('duration').setDescription('how long, in hours, the poll should remain active.')),
+  async execute(interaction) {
     await interaction.deferReply();
-    await interaction.followUp(`Under construction`)
+    const message = {
+      poll: {
+        question: {
+          text: interaction.options.getString('question')
+        },
+        duration: interaction.options.getNumber('duration') ?? 1, // duration in hours, minimum 1
+        allowMultiselect: interaction.options.getBoolean('multiple') ?? false
+      },
+    }
+    const choices = interaction.options.getString('choices');
+    if (choices) {
+      const answers = choices.split(',');
+      message.poll.answers = [];
+      for (let i = 0; i < answers.length; i++) {
+        message.answers.push({ text: answers[i].trim(), emoji: emojiCharacters[i + 1] });
+      }
+    } else {
+      message.poll.answers = [{ text: 'Yes', emoji: `👍` }, { text: 'No', emoji: `👎`}];
+    }
+    console.log(message);
+    await interaction.followUp({ content: `Poll started by ${interaction.user.id}`, poll: message.poll })
   }
 }
