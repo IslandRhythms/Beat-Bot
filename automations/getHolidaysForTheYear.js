1283d0dfa3f1a45f27ff3e5e46dd42f263579bc0
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2024-05-10 22:39:10 -0400

    add try catch

--- automations/getHolidaysForTheYear.js
+++ automations/getHolidaysForTheYear.js
@@ -5,22 +5,26 @@ const fs = require('fs/promises');
 const events = require('../resources/Events.json');
 
 module.exports = async function getHolidaysForTheYear() {
-  const res = await axios.get(`https://date.nager.at/api/v3/PublicHolidays/2024/US`).then(res => res.data);
-  const fixedHolidays = res.map(x => ({ date: new Date(x.date).toISOString(), name: x.name }))
-  const year = new Date().getFullYear();
-  for (let i = 0; i < events.length; i++) {
-    events[i].date = new Date(`${year}-${events[i].date}`).toISOString();
-  }
-  const thanksgiving = fixedHolidays.find(x => x.name == 'Thanksgiving Day');
-  const thanksDate = new Date(thanksgiving.date);
-  const blackFriday = new Date(thanksDate.setDate(thanksDate.getDate() + 1));
-  events.push({ name: 'Black Friday', date: blackFriday.toISOString()})
-  const holidays = fixedHolidays.concat(events).sort(function(a,b) {
-    if (a.date < b.date) {
-      return -1;
-    } else {
-      return 1;
+  try {
+    const res = await axios.get(`https://date.nager.at/api/v3/PublicHolidays/2024/US`).then(res => res.data);
+    const fixedHolidays = res.map(x => ({ date: new Date(x.date).toISOString(), name: x.name }))
+    const year = new Date().getFullYear();
+    for (let i = 0; i < events.length; i++) {
+      events[i].date = new Date(`${year}-${events[i].date}`).toISOString();
     }
-  })
-  await fs.writeFile(`./resources/holidays.json`, JSON.stringify(holidays, null, 2));
+    const thanksgiving = fixedHolidays.find(x => x.name == 'Thanksgiving Day');
+    const thanksDate = new Date(thanksgiving.date);
+    const blackFriday = new Date(thanksDate.setDate(thanksDate.getDate() + 1));
+    events.push({ name: 'Black Friday', date: blackFriday.toISOString()})
+    const holidays = fixedHolidays.concat(events).sort(function(a,b) {
+      if (a.date < b.date) {
+        return -1;
+      } else {
+        return 1;
+      }
+    })
+    await fs.writeFile(`./resources/holidays.json`, JSON.stringify(holidays, null, 2));
+  } catch (error) {
+    console.log('something went wrong while getting the holidays for the year', error)
+  }
 }
